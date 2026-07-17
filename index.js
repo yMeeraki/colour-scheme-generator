2a1497d3c58683299ac78da773892e990d5aadf0
Yukti Gupta <yuktigupta95@gmail.com>
2026-07-17 18:13:20 +0530

    refactor: add id to color section container for improved DOM manipulation

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-fetch(
-  "https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic&count=5",
-)
+fetch("https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic&count=5")
   .then((res) => res.json())
   .then((data) => console.log(data));
+
+const colorCountainer = document.querySelector("#color-section-container");
+
+function colorDOM() {
+  return `<div>
+          <div class="color red"></div>
+          <div class="hex-color">#ffff</div>
+        </div>`;
+}
